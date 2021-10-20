b7c8861468b29a53c06711378c4079fec6b32b51
Vatsal Rupamkumar Tikiwala <60180639+Vatsal3631@users.noreply.github.com>
2021-10-20 16:28:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,152 @@
+# importing necessary libraries and functions
+import numpy as np
+from flask import Flask, request, jsonify, render_template, send_file, redirect, url_for
+from sr_class import SR
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import imquality.brisque as brisque
+import tensorflow as tf
+from os.path import isdir
+import shutil
+from tensorflow.keras.preprocessing.image import array_to_img
+from tensorflow.keras.preprocessing.image import save_img
+from flask_share import Share
+share = Share()
+
+app = Flask(__name__) #Initialize the flask App
+share.init_app(app)
+#model = pickle.load(open('model.pkl', 'rb')) # loading the trained model
+
+
+@app.route('/') # Homepage
+def home():
+    return render_template('index.html')
+    
+UPLOAD_FOLDER = 'uploads/'    
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+  
+    
+@app.route('/upScale',methods=['Get','POST'])
+def upScale():
+    file = request.files['file_name']
+    quality = request.form['quality']
+    if quality == "Base":
+        scale = request.form['scale']
+    else: 
+        scale = 1
+    if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        size, fs_img, Final_Img = upScale_image(file,quality,scale)
+        image = cv2.imread(app.config['UPLOAD_FOLDER']+"/Image.jpg")
+        image = cv2.resize(np.asarray(image), size)
+        acc0 = brisque.score(image)
+        #fs_img = cv2.imread("./static/downloads/enhanced_photo01.jpg")
+        acc1 = acc0 - brisque.score(fs_img) 
+        acc2 = acc0 - brisque.score(Final_Img)
+        return render_template('index1.html',prediction_text=round(acc1,4),prediction_text1=round(acc2,4))
+    elif file.filename.lower().endswith(('.3gp', '.mp4', '.avi', '.mkv')):
+        size = upScale_video(file,quality,scale)
+        #cap = cv2.VideoCapture(app.config['UPLOAD_FOLDER']+"/Video.mp4")
+        acc0 = frame_generator(app.config['UPLOAD_FOLDER']+"/Video.mp4",name='image000.jpg',size=size,save_img=False)
+        #cap.release()
+        #cap = cv2.VideoCapture("./static/downloads/fsrcnn_output.avi")
+        score = frame_generator("./static/downloads/fsrcnn_output.avi",name='fsrcnn_image.jpg',size=size,save_img=True)
+        acc1 = acc0 - score
+        #cap.release()
+        #cap = cv2.VideoCapture("./static/downloads/srgan_output.avi")
+        score = frame_generator("./static/downloads/srgan_output.avi",name='srgan_image.jpg',size=size,save_img=True)
+        acc2 = acc0 - score
+        #cap.release()
+        return render_template('index2.html',prediction_text=round(acc1,4),prediction_text1=round(acc2,4))
+
+def frame_generator(path,name,size,save_img):
+    cap = cv2.VideoCapture(path)
+    ret,frame=cap.read()
+    image = cv2.resize(frame, size)
+    if save_img:
+        path = 'static/downloads/'
+        cv2.imwrite(os.path.join(path , name), image)
+    cap.release()
+    return brisque.score(image)
+
+def upScale_video(vid,quality,scale):
+    vid.save(os.path.join(app.config['UPLOAD_FOLDER'], "Video.mp4"))
+    cam = cv2.VideoCapture(app.config['UPLOAD_FOLDER']+"/Video.mp4") 
+    #fps = cam.get(cv2.CAP_PROP_FPS)
+    if quality == "144p":
+        size = (256,144)
+    elif quality == "240p":
+        size = (426,240)
+    elif quality == "360p":
+        size = (480,360)
+    elif quality == "480p":
+        size = (640,480)
+    sr = SR()
+    s_res = sr.res_video(cam) 
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH) * int(scale))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT) * int(scale)) #int(cap.get(4))
+    #height, width, layers = img.shape
+    s1 = (frame_width,frame_height)
+    if quality == "Base":
+        sr.fsr_video(size=s1)
+        size = s1
+    else:
+        sr.fsr_video(size=size)
+        size = size    
+    fps = 20       
+    out = cv2.VideoWriter('./static/downloads/srgan_output.avi',cv2.VideoWriter_fourcc(*'MJPG'), fps , size)
+    for i in range(len(s_res)):
+        out.write(cv2.resize(cv2.imread(s_res[i]), size))
+    out.release()
+    return size
+
+def upScale_image(img,quality,scale):
+    '''
+    For rendering results on HTML GUI
+    '''
+    sr = SR()
+    sr.init_super("FSRCNN_x4")
+    model = sr.srgan()   #generator()
+    model.load_weights("gan_generator.h5")
+    filename = secure_filename(img.filename)
+    img.save(os.path.join(app.config['UPLOAD_FOLDER'], "Image.jpg"))
+    if quality == "144p":
+        size = (256,144)
+    elif quality == "240p":
+        size = (426,240)
+    elif quality == "360p":
+        size = (480,360)
+    elif quality == "480p":
+        size = (640,480)
+    image = cv2.imread(app.config['UPLOAD_FOLDER']+"/Image.jpg")
+    width = int(image.shape[1] * int(scale))
+    height = int(image.shape[0] * int(scale))
+    print(image.shape[1])
+    s1 = (width,height)
+    sres = sr.resolve_single(model, image)
+    if quality == "Base":
+        fs_img = sr.super_res(image, name= 'enhanced_photo01.jpg', save_img=True, scale=True, size=s1)
+        Final_Img = cv2.resize(np.asarray(sres),s1)
+        size = s1
+    else:
+        fs_img = sr.super_res(image, name= 'enhanced_photo01.jpg', save_img=True, scale=False, size=size)
+        Final_Img = cv2.resize(np.asarray(sres),size)
+        size = size
+            
+    path = 'static/downloads/enhanced_photo.jpg'
+    cv2.imwrite(path, Final_Img)
+    return size, fs_img, Final_Img
+    
+
+@app.route('/static/downloads')
+def downloads():
+    filename = "./static/downloads/enhanced_photo.jpg"
+    return send_file(filename, as_attachment=True)
+
+@app.route('/static')    
+def download_video():
+    filename = "./static/downloads/srgan_output.avi"
+    return send_file(filename, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ sr_class.py
@@ -0,0 +1,195 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from cv2 import dnn_superres
+import tensorflow as tf
+from utils import load_image, plot_sample
+from os.path import isdir
+import shutil
+from tensorflow.keras.preprocessing.image import array_to_img
+from tensorflow.keras.preprocessing.image import save_img
+from PIL import Image
+import tensorflow as tf
+from tensorflow.python.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
+from tensorflow.python.keras.models import Model
+from utils import load_image, plot_sample
+from IPython import get_ipython
+#get_ipython().run_line_magic('cd', 'contact-form-03\\')
+#%cd Mini Project II/Final
+
+
+#@app.route('/upscale',methods=['POST'])
+#print(os.getcwd())
+class SR:
+    def init_super(self, model, base_path='Model'):
+        global sr, model_name, model_scale      # Define global variable
+
+        sr = dnn_superres.DnnSuperResImpl_create()   # Create an SR object
+
+        model_path = os.path.join(model +".pb")    # Define model path
+
+        model_name = model.split('_')[0].lower()     # Extract model name from model path
+
+        model_scale = int(model.split("_")[1][1])    # Extract model scale from model path
+
+        sr.readModel(model_path)    # Read the desired model
+
+        sr.setModel(model_name, model_scale)
+
+        
+    
+    def super_res(self, image, returndata=False, save_img=True, name='test.png', size=(256,144), scale=True):
+        #image = cv2.imread("uploads/Image.jpg")
+        #print(type(img))
+        Final_Img = sr.upsample(image)     # Upscale the image
+        if  returndata:
+            return Final_Img
+
+        else:
+
+            if save_img:
+                if scale:
+                    Final_Img = cv2.resize(Final_Img,size)
+                else:
+                    Final_Img = cv2.resize(Final_Img,size)
+                path = 'static/downloads/'
+                cv2.imwrite(os.path.join(path , name), Final_Img)
+                #cv2.imwrite("{{ url_for('predict'),filename=}}" + name, Final_Img)
+        return Final_Img
+
+    def fsr_video(self,size):
+        fps=0
+        self.init_super("FSRCNN_x4")
+        cap = cv2.VideoCapture("./uploads/Video.mp4")
+        size = size #(frame_width, frame_height) #(640,480)
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        out = cv2.VideoWriter('./static/downloads/fsrcnn_output.avi', fourcc, 20, size)
+
+        while(cap.isOpened()):    
+            
+            ret,frame=cap.read() 
+            
+            if ret == True:
+                image = cv2.flip(frame,180)
+                
+                image = cv2.flip(image,1)
+            
+                image = self.super_res(image, returndata=True, save_img=False, size=size) 
+            
+                #cv2.putText(image, 'FPS: {:.2f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,0.8, (255, 20, 55), 1)
+                #cv2.imshow("Super Resolution", image)
+                
+                out.write(cv2.resize(image, size))
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                    
+            else:
+                break
+
+        cap.release() 
+        out.release()
+        cv2.destroyAllWindows()  
+        
+    def res_video(self,cam):
+        try:
+            if not os.path.exists('./uploads/data'): 
+                os.makedirs('./uploads/data') 
+        except OSError:
+            print ('Error: Creating directory of data') 
+      
+        currentframe = 0
+        arr_img = []
+        while(True): 
+            ret,frame = cam.read() 
+            if ret: 
+                name = './uploads/data/frame' + str(currentframe).zfill(3) + '.jpg'
+                #print ('Creating...' + name) 
+                cv2.imwrite(name, frame) 
+                currentframe += 1
+                arr_img.append(name)
+            else: 
+                break
+        model = self.srgan()   #generator()
+        model.load_weights('gan_generator.h5')
+
+        arr_output=[]
+        n= len(arr_img)
+
+        for i in range(n):
+            lr = load_image(arr_img[i])
+            sres = self.resolve_single(model, lr)
+            arr_output.append(sres)
+        cam.release() 
+        cv2.destroyAllWindows()
+        
+        if isdir("./static/downloads/output_images"):
+            shutil.rmtree("./static/downloads/output_images")
+        os.makedirs("./static/downloads/output_images")
+        s_res= []
+        for j in range(len(arr_output)):
+            out_name = './static/downloads/output_images/frame' + str(j).zfill(3) + '.jpg'
+            img_pil = array_to_img(arr_output[j])
+            img1 = save_img(out_name, img_pil)
+            s_res.append(out_name)
+        return s_res
+    
+    def resolve_single(self,model, lr):
+        return self.resolve(model, tf.expand_dims(lr, axis=0))[0]  
+    
+    def resolve(self,model, lr_batch):
+        lr_batch = tf.cast(lr_batch, tf.float32)
+        sr_batch = model(lr_batch)
+        sr_batch = tf.clip_by_value(sr_batch, 0, 255)
+        sr_batch = tf.round(sr_batch)
+        sr_batch = tf.cast(sr_batch, tf.uint8)
+        return sr_batch
+
+    def pixel_shuffle(self,scale):
+        return lambda x: tf.nn.depth_to_space(x, scale)
+
+    def normalize_01(self,x):
+        """Normalizes RGB images to [0, 1]."""
+        return x / 255.0
+
+    def denormalize_m11(self,x):
+        """Inverse of normalize_m11."""
+        return (x + 1) * 127.5
+
+    def res_block(self,x_in, num_filters, momentum=0.8):
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+        x = BatchNormalization(momentum=momentum)(x)
+        x = PReLU(shared_axes=[1, 2])(x)
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+        x = BatchNormalization(momentum=momentum)(x)
+        x = Add()([x_in, x])
+        return x
+
+    def upsample(self,x_in, num_filters):
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+        x = Lambda(self.pixel_shuffle(scale=2))(x)
+        return PReLU(shared_axes=[1, 2])(x)
+
+    def srgan(self,num_filters=64, num_res_blocks=16):
+        x_in = Input(shape=(None, None, 3))
+        x = Lambda(self.normalize_01)(x_in)
+
+        x = Conv2D(num_filters, kernel_size=9, padding='same')(x)
+        x = x_1 = PReLU(shared_axes=[1, 2])(x)
+
+        for _ in range(num_res_blocks):
+            x = self.res_block(x, num_filters)
+
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Add()([x_1, x])
+
+        x = self.upsample(x, num_filters * 4)
+        x = self.upsample(x, num_filters * 4)
+
+        x = Conv2D(3, kernel_size=9, padding='same', activation='tanh')(x)
+        x = Lambda(self.denormalize_m11)(x)
+
+        return Model(x_in, x)
